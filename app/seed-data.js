db9b3b2cd3498190c991b0eac1f79978b50af0ac
irosgrim <irosgrim@gmail.com>
2018-11-02 12:00:01 +0100

    change seed DB

--- app/seed-data.js
+++ app/seed-data.js
@@ -1,7 +1,7 @@
 let db=[{id: 0, isDone: false, note: 'Hello world', date: dateToday()},
-        {id: 1, isDone: false, note: 'It was halloween today', date: dateToday()},
-        {id: 2, isDone: true, note: 'Tomorrow is christmas', date: dateToday()},
-        {id: 3, isDone: true, note: 'My name is Ion', date: dateToday()}
+        {id: 1, isDone: false, note: 'Second note', date: dateToday()},
+        {id: 2, isDone: true, note: 'Third note', date: dateToday()},
+        {id: 3, isDone: true, note: 'Fourth note', date: dateToday()}
     ];
 
 
